58f634098af4bd0bcb7c8b55615400896c37991a
Samuel <samolayemi3@gmail.com>
2024-11-14 15:09:07 +0100

    Fixed Nextjs configurations

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,16 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: [
-        'media.istockphoto.com',
-        'images.unsplash.com',
-        'plus.unsplash.com',
-        'ipfs.io',
-        'gateway.pinata.cloud'
-        // Add any other domains you need to load images from
-      ],
-    },
-    reactStrictMode: true,
-  }
+  images: {
+    domains: [
+      'media.istockphoto.com',
+      'images.unsplash.com',
+      'plus.unsplash.com',
+      'ipfs.io',
+      'gateway.pinata.cloud',
+      // Add any other domains you need to load images from
+    ],
+  },
+  reactStrictMode: true,
+  
+  // Add this line to enable static exports
+  output: 'export',
+};
 
 export default nextConfig;
